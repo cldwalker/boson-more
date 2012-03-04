7a7db42bf91a694d981069360b8cfebab6a75e83
Gabriel Horner <gabriel.horner@gmail.com>
2012-03-04 11:07:36 -0500

    don't use a hardcoded local path :bomb:

--- lib/boson/more_commands.rb
+++ lib/boson/more_commands.rb
@@ -2,10 +2,13 @@ require 'boson/commands/core'
 require 'boson/commands/web_core'
 require 'boson/commands/view_core'
 require 'boson/runner'
+require 'fileutils'
 
 Boson::BareRunner::DEFAULT_LIBRARIES << Boson::Commands::Core
 Boson::BareRunner::DEFAULT_LIBRARIES << Boson::Commands::WebCore
 Boson::BareRunner::DEFAULT_LIBRARIES << Boson::Commands::ViewCore
-# dir = File.dirname(File.readlink(__FILE__))
-dir = File.expand_path "~/code/gems/boson-all/lib/boson"
+
+# TODO: Use Boson.home when it exists
+dir = Dir.home + '/.boson/.more_commands'
+FileUtils.mkdir_p dir
 Boson.repos << Boson::Repo.new(dir)
