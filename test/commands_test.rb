97ca8ebbe5d405d3ff7302e53969d5717c611925
Gabriel Horner <gabriel.horner@gmail.com>
2012-03-13 19:53:31 -0400

    made test bacon compatible, maybe keep?

--- test/commands_test.rb
+++ test/commands_test.rb
@@ -1,53 +1,53 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
+require 'boson/more_commands'
+require 'boson/view'
 
-module Boson
-  class CommandsTest < Test::Unit::TestCase
-    before(:all) {
-        @higgs = Boson.main_object
-        if Boson.libraries.size.zero?
-          reset_boson
-          ancestors = class <<Boson.main_object; self end.ancestors
-          # allows running just this test file
-          Manager.load Runner.default_libraries unless ancestors.include?(Boson::Commands::Core)
-        end
+describe "commands" do
+  before_all do
+      @higgs = Boson.main_object
+      if Boson.libraries.size.zero?
+        reset_boson
+        ancestors = class <<Boson.main_object; self end.ancestors
+        # allows running just this test file
+        Manager.load BareRunner.default_libraries unless ancestors.include?(Boson::Commands::Core)
+      end
+  end
+
+  def render_expects(&block)
+    Boson::View.expects(:render).with(&block)
+  end
+
+  describe "libraries" do
+    before_all {
+      Boson.libraries << Boson::Library.new(:name=>'blah')
+      Boson.libraries << Boson::Library.new(:name=>'another', :module=>"Cool")
     }
 
-    def render_expects(&block)
-      View.expects(:render).with(&block)
+    it "lists all when given no argument" do
+      render_expects {|*args| args[0].size == Boson.libraries.size }
+      @higgs.libraries
     end
 
-    context "libraries" do
-      before(:all) {
-        Boson.libraries << Boson::Library.new(:name=>'blah')
-        Boson.libraries << Boson::Library.new(:name=>'another', :module=>"Cool")
-      }
+    it "searches with a given search field" do
+      render_expects {|*args| args[0] == [Boson.library('another')]}
+      @higgs.libraries('Cool', :query_fields=>[:module])
+    end
+  end
 
-      test "lists all when given no argument" do
-        render_expects {|*args| args[0].size == Boson.libraries.size }
-        @higgs.libraries
-      end
+  describe "commands" do
+    before_all {
+      Boson.commands << Command.create('some', Library.new(:name=>'thing'))
+      Boson.commands << Command.create('and', Library.new(:name=>'this'))
+    }
 
-      test "searches with a given search field" do
-        render_expects {|*args| args[0] == [Boson.library('another')]}
-        @higgs.libraries('Cool', :query_fields=>[:module])
-      end
+    it "lists all when given no argument" do
+      render_expects {|*args| args[0].size == Boson.commands.size }
+      @higgs.commands
     end
 
-    context "commands" do
-      before(:all) {
-        Boson.commands << Command.create('some', Library.new(:name=>'thing'))
-        Boson.commands << Command.create('and', Library.new(:name=>'this'))
-      }
-
-      test "lists all when given no argument" do
-        render_expects {|*args| args[0].size == Boson.commands.size }
-        @higgs.commands
-      end
-
-      test "searches with a given search field" do
-        render_expects {|*args| args[0] == [Command.find('and')]}
-        @higgs.commands('this', :query_fields=>[:lib])
-      end
+    it "searches with a given search field" do
+      render_expects {|*args| args[0] == [Command.find('and')]}
+      @higgs.commands('this', :query_fields=>[:lib])
     end
   end
 end
